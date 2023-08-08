251d1b62b5366dde0ecd5e7062debaebc6db8a08
Felipe Marin <luisf.maring0910@gmail.com>
2023-08-07 21:16:19 -0500

    ccip send an reciver init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
+Example contracts
+
+CCIPReceiver_Unsafe deployed to 0x1582702fDBf5dBbA4191D3dA70f6681f9AcC6E22
+CCIPSender_Unsafe deployed to 0xff67AC887dfED5212091C7b0B11b6D02BB17D19F
+
+hash tx to 0x97c9d96c781efd13a87f961dd206346fac791781ed1b21aee96b4b05e2496282
+
+https://ccip.chain.link/msg/0xabd92b91fd1b519d5f4397158b6fbf4935c463434fc4c574c856df2555ca8499
---
+++ scripts/sendMessage.ts
@@ -0,0 +1,34 @@
+// scripts/sendMessage.ts
+
+import { ethers, network } from "hardhat";
+
+async function main() {
+  if(network.name !== `avalancheFuji`) {
+    console.error(`❌ Must be called from Avalanche Fuji`);
+    return 1;
+  }
+
+  const ccipSenderAddress = `0xff67AC887dfED5212091C7b0B11b6D02BB17D19F`;
+  const ccipReceiverAddress = `0x1582702fDBf5dBbA4191D3dA70f6681f9AcC6E22`;
+  const someText = `CCIP Masterclass`;
+  const destinationChainSelector = ethers.BigNumber.from("16015286601757825753");
+
+  const ccipSenderFactory = await ethers.getContractFactory( "CCIPSender_Unsafe");
+  const ccipSender = await ccipSenderFactory.attach(ccipSenderAddress)
+  ccipSender.connect(ethers.provider);
+  //const ccipSender = await ccipSenderFactory.connect(ccipSenderAddress, ethers.provider);
+
+  const tx = await ccipSender.send(
+      ccipReceiverAddress, 
+      someText,
+      destinationChainSelector, 
+      {gasLimit: 200000}
+  );
+
+  console.log(`Transaction hash: ${tx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
